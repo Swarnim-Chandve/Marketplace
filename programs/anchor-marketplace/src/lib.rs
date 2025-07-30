e7bd83d7325734159e58dcb1cfdc417b3cbbb409
SWARNIM <swarnimchandve23@gmail.com>
2025-07-31 00:43:21 +0530

    Added Marketplace logic and tests

---
+++ programs/anchor-marketplace/src/lib.rs
@@ -0,0 +1,40 @@
+#![allow(unexpected_cfgs, deprecated)]
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("Eoh5f4UZg6cvKBa7pMGcudUC8xsANPSNWXHQeCTeZE7x");
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize_marketplace(ctx: Context<InitializeMarketplace>, fee_percentage: u8) -> Result<()> {
+        ctx.accounts.initialize_marketplace(fee_percentage, ctx.bumps)?;
+        Ok(())
+    }
+
+
+    pub fn list_nft(ctx: Context<ListNft>, price: u64) -> Result<()> {
+        ctx.accounts.initialize_listing(price, ctx.bumps)?;
+        ctx.accounts.transfer_nft()
+    }
+
+
+    pub fn delist_nft(ctx: Context<DelistNft>) -> Result<()> {
+        ctx.accounts.transfer_back_nft()
+    }
+
+    pub fn purchase_nft(ctx: Context<PurchaseNft>) -> Result<()> {
+        ctx.accounts.transfer_nft()?;
+        ctx.accounts.transfer_sol()?;
+        ctx.accounts.delist_nft()
+    }
+}
